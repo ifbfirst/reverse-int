0ade3a0d66f83a2fcae144afe277bc59692e61f5
Kate Mashko <katika.life@gmail.com>
2023-08-06 09:26:58 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let i = 0;
+  let nAbs = Math.abs(n);
+   let nStr = String(nAbs);
+  let result = '';
+while (i < nStr.length){
+ result = `${nStr[i]}${result}`;
+ i = i+1;
+}
+return Number(result);
 }
